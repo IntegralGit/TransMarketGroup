82a7518264c446c91af74b3c6ab689b2592cb56a
Piotr Nowak <piotr.nowak@nokia.com>
2013-08-22 15:04:44 -0500

    First Class Push

---
+++ tmg/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: rotten
+ * Date: 8/22/13
+ * Time: 3:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+
+    public static void main (String[] args){
+
+        System.out.println("TMG IS AWESOME");
+
+    }
+}
