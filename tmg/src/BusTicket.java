13c2aa889c2f9774d1ef4115cf8ac5b0e990b8d7
Piotr Nowak <piotr.nowak@nokia.com>
2013-08-22 15:33:42 -0500

    isLucky Bus Ticket complete -will review at end

---
+++ tmg/src/BusTicket.java
@@ -0,0 +1,48 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: rotten
+ * Date: 8/22/13
+ * Time: 3:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BusTicket {
+
+    private static int[] BusTickets = new int[5];
+
+
+    public boolean isLucky(int x[]){
+        int A = 0;
+        int B = 0;
+
+        for (int i = 0; i < x.length;++i)
+        {
+
+
+            if (i < 3)
+                A += x[i];
+            else
+                B += x[i];
+        }
+
+        if (A == B)
+           return true;
+        else
+           return false;
+
+
+
+    }
+
+
+
+    public static void main (String[] args){
+
+        int arr[] = {1,2,3,3,2,1};
+        BusTicket a = new BusTicket();
+        System.out.print(a.isLucky(arr));
+
+
+    }
+
+
+}
